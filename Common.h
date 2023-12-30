385dcd12898c262f67ab730cfd792b66cfca90f1
jps1973 <47426983+jps1973@users.noreply.github.com>
2023-12-30 19:26:38 +0000

    Initial commit

---
+++ Common.h
@@ -0,0 +1,15 @@
+// Common.h
+
+#pragma once
+
+#include <windows.h>
+
+#define STRING_LENGTH															2048
+
+#define ALL_FILES_FILTER														"*.*"
+#define NEW_LINE_TEXT															"\r\n"
+
+#define ABOUT_MESSAGE_CAPTION													"About"
+#define ERROR_MESSAGE_CAPTION													"Error"
+#define INFORMATION_MESSAGE_CAPTION												"Information"
+#define WARNING_MESSAGE_CAPTION													"Warning"
